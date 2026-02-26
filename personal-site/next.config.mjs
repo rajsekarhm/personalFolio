a073e3e833fda032f5014b0562911450c1413f82
rajasekar.hm <rajasekar.hm@zohocorp.com>
2026-02-26 19:47:08 +0530

    Refactor code structure for improved readability and maintainability

--- personal-site/next.config.mjs
+++ personal-site/next.config.mjs
@@ -1,19 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint:{
-      ignoreDuringBuilds:true
-    },
     typescript:{
         ignoreBuildErrors:true
     },
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.(mov|mp4|webm|ogg|avi|wmv|flv)$/,
-        type: "asset/resource",
-      });
-  
-      return config;
-    },
+    turbopack: {},
 };
 
 export default nextConfig;
